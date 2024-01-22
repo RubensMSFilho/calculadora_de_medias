9622bd88b130a1dd60056380d31256f4f83e05af
Rubens Monteiro <rubens.msfilh@hotmail.com>
2024-01-22 16:36:23 -0300

    a

--- Galeria de fotos/main.js
+++ Galeria de fotos/main.js
@@ -8,9 +8,18 @@ $(document).ready(function() {
    })
 
    $('form').on('submit', function(e) {
-    console.log('submit');
     e.preventDefault();
-    
+    const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+    const novoItem = $('<li></li>');
+    $(`<img src="$(enderecoDaNovaImagem)" />`).appendTo(novoItem);
+    $(`
+    <div class="overlay-imagem-link" />
+        <a href="$(enderecoDaNovaImagem)" target="_black" title="Ver imagem no tamanho real">
+        Ver imagem no tamanho real
+        </a>
+        </div>
+    `).appendTo(novoItem);
+    $(novoItem).appendTo('ul');
    })
 
 })
